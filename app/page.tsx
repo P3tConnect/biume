6017708e3bd18893975a746bbeb84b7ccee79983
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-05-27 21:24:40 +0200

    add text white

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <Input
           type="text"
           placeholder="john.doe@icloud.com"
-          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4 text-white bg-neutral-950 placeholder:text-neutral-700"
         />
       </div>
       <BackgroundBeams />
