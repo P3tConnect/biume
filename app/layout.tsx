414ac645f2227fcd75a7f66b740844856c96677a
FrekiManagarm <mathchambaud@icloud.com>
2024-05-31 09:49:13 +0200

    add speed insights and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/src/lib/utils";
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from "@/src/context/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -46,6 +48,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
