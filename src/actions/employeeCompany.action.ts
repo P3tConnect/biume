cd844cfe37c5e3dd660d15842a2cddcd72d9b477
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-06-19 18:11:20 +0200

    fix merge

---
+++ src/actions/employeeCompany.action.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { z } from "zod";
+import { CreateEmployeeCompanySchema } from "../db";
+import { proAction } from "../utils/action";
+
+export async function getEmployeesCompany() {}
+
+export const createEmployeeCompany = proAction(
+  CreateEmployeeCompanySchema,
+  async (params, _) => {},
+);
+
+export const updateEmployeeCompany = proAction(
+  CreateEmployeeCompanySchema,
+  async (params, _) => {},
+);
+
+export const deleteEmployeeCompany = proAction(
+  z.string(),
+  async (params, _) => {},
+);
---
+++ src/actions/intolerences.action.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { z } from "zod";
+import { CreateIntolerenceSchema } from "../db";
+import { userAction } from "../utils/action";
+
+export async function getIntolerences() {}
+
+export const createIntolerence = userAction(
+  CreateIntolerenceSchema,
+  async (params, _) => {},
+);
+
+export const updateIntolerence = userAction(
+  CreateIntolerenceSchema,
+  async (params, _) => {},
+);
+
+export const deleteIntolerence = userAction(
+  z.string(),
+  async (params, _) => {},
+);
