c811878add5bba31097e361f9a7d7d4100332fbf
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-07-01 21:52:21 +0200

    some modifs

--- emails/NewPersonWaitList.tsx
+++ emails/NewPersonWaitList.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { EmailLayout } from './EmailLayout'
-import { Container } from '@react-email/components'
+import { Container, Text } from '@react-email/components'
 
 const NewPersonWaitList = ({ subEmail }: { subEmail: string }) => {
   return (
     <EmailLayout>
       <Container>
-        <h1>Hi, you have a new subscriber</h1>
+        <Text className=''>Hi, you have a new subscriber</Text>
         Email : <p>{subEmail}</p>
       </Container>
     </EmailLayout>
