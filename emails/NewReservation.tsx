655d854040ec96b697fc4c0972670a6b77a31b61
FrekiManagarm <mathchambaud@icloud.com>
2024-07-03 14:25:12 +0200

    feat(pages): Add pages and emails --------------------------------------- Add more pages Add more emails --------------------------------------- Reviewed By : Mathieu Chambaud

---
+++ emails/NewReservation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { EmailLayout } from './EmailLayout'
+
+const NewReservation = () => {
+  return (
+    <EmailLayout>NewReservation</EmailLayout>
+  )
+}
+
+export default NewReservation
