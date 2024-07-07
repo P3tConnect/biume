67c185ba9517785d16813ae268438de1d719ddd0
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-07-07 18:28:56 +0200

    add some pages to begin the project

---
+++ app/(main)/dashboard/@pro/management/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProDashboardManagementPage = () => {
+  return (
+    <div>ProDashboardManagementPage</div>
+  )
+}
+
+export default ProDashboardManagementPage
---
+++ app/(main)/error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPageError = () => {
+  return (
+    <div>
+      Something went wrong
+    </div>
+  )
+}
+
+export default LandingPageError
