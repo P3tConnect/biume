29526fad5d894f916748fe692e3b12b3bc3a016e
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-07-22 07:52:55 +0200

    move folders and  modify source of db

--- app/(main)/dashboard/@admin/subscriptions/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SubscriptionsDashboardPage = () => {
-  return (
-    <div>SubscriptionsDashboardPage</div>
-  )
-}
-
-export default SubscriptionsDashboardPage
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "../db";
+import { safeConfig } from "./env";
 
-const sql = neon(
-  "postgresql://pawthera_db_owner:P8OQM9LHxlUp@ep-misty-firefly-a2bm78th.eu-central-1.aws.neon.tech/pawthera_db?sslmode=require",
-);
+const sql = neon(safeConfig.DATABASE_URL);
 export const db = drizzle(sql, { schema });
