5b497cfc8d99517f4c41e2000ab88e1b72efca92
FrekiManagarm <mathchambaud@icloud.com>
2024-08-28 13:20:21 +0200

    dashboard layout WIP

--- components/dashboard/layout/dashboard-layout.tsx
+++ components/dashboard/layout/dashboard-layout.tsx
@@ -16,18 +16,18 @@ export default function DashboardLayout({
   if (!sidebar) return null;
 
   return (
-    <>
-      <Sidebar />
-      <main
-        className={cn(
-          "min-h-[calc(100vh_-_56px)] transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
-        )}
-      >
-        {/* <ContentLayout title="Dashboard"> */}
-          {children}
-        {/* </ContentLayout> */}
-      </main>
-    </>
+    <div className="p-5 h-[100vh] w-[100vw] justify-center items-center">
+      <div className="flex flex-row h-full w-full justify-start items-center pt-5 pb-5 bg-background/90 backdrop-blur-xl rounded-2xl border border-border">
+          <Sidebar />
+          <main
+            className=
+              "min-h-[calc(100vh_-_56px)] transition-[margin-left] ease-in-out duration-300"
+          >
+            {/* <ContentLayout title="Dashboard"> */}
+              {children}
+            {/* </ContentLayout> */}
+          </main>
+      </div>
+    </div>
   );
 }
--- components/dashboard/layout/sidebar.tsx
+++ components/dashboard/layout/sidebar.tsx
@@ -16,14 +16,11 @@ export function Sidebar() {
   if(!sidebar) return null;
 
   return (
-    <div
-      className={cn(
-        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
-      )}
-    >
+    <div className={cn("w-72 h-full bg-background m-5 rounded-xl -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300 border border-border",
+      sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+    )}>
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
-      <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
+      <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto">
         <Button
           className={cn(
             "transition-transform ease-in-out duration-300 mb-1",
@@ -50,4 +47,16 @@ export function Sidebar() {
       </div>
     </div>
   );
+
+  // return (
+  //   <div
+  //     className={cn(
+  //       "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+  //       sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+  //     )}
+  //   >
+  //     <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
+  //     
+  //   </div>
+  // );
 }
