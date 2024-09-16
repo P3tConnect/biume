9c28894190826365e8535c4fdb041e71fe3cea52
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-09-16 19:59:05 +0200

    redeploy

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,5 +1,5 @@
 import { createSafeActionClient } from "next-safe-action";
-import { currentUser } from "./current-user";
+// import { currentUser } from "./current-user";
 
 export class ActionError extends Error {
   constructor(message: string) {
@@ -17,40 +17,41 @@ const handleReturnedServerError = (error: Error) => {
 };
 
 export const action = createSafeActionClient({
-  handleReturnedServerError: handleReturnedServerError,
+  handleServerError: handleReturnedServerError,
 });
 
 export const userAction = action.use(async ({ next }) => {
-  const user = await currentUser();
+  // const user = await currentUser();
 
-  if (!user) {
-    throw new ActionError("You must be logged in !");
-  }
+  // if (!user) {
+  //   throw new ActionError("You must be logged in !");
+  // }
 
-  return next({ ctx: user });
+  return next({ ctx: undefined });
 });
 
 export const proAction = action.use(async ({ next }) => {
-  const user = await currentUser();
+  // const user = await currentUser();
 
-  if (!user) {
-    throw new ActionError("You must be logged in !");
-  }
+  // if (!user) {
+  //   throw new ActionError("You must be logged in !");
+  // }
 
-  if (user.plan != "NONE") {
-    return next({ ctx: user });
-  }
+  // if (user.plan != "NONE") {
+  //   return next({ ctx: user });
+  // }
+  return next({ ctx: undefined });
 
-  throw new ActionError(
-    "You need to subscribe to a plan to perform this action",
-  );
+  // throw new ActionError(
+  //   "You need to subscribe to a plan to perform this action",
+  // );
 });
 
-// export const adminAction = action.use(async ({ next }) => {
-//   const user = await currentUser();
+export const adminAction = action.use(async ({ next }) => {
+  // const user = await currentUser();
 
-//   if (!user) {
-//     throw new ActionError("You must be logged in !")
-//   }
-
-// });
+  // if (!user) {
+  //   throw new ActionError("You must be logged in !")
+  // }
+  return next({ ctx: undefined });
+});
