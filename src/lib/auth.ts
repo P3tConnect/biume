55d484feb74e0e23feb2328258cb102ca2ef490a
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-09-16 19:07:16 +0200

    redeploy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,13 +8,13 @@ import { verificationTokens } from "../db/verificationToken";
 import { authenticators } from "../db/verificators";
 import { Adapter } from "next-auth/adapters";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable: user,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-    authenticatorsTable: authenticators,
-  }) as Adapter,
-  providers: [],
-});
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   adapter: DrizzleAdapter(db, {
+//     usersTable: user,
+//     accountsTable: accounts,
+//     sessionsTable: sessions,
+//     verificationTokensTable: verificationTokens,
+//     authenticatorsTable: authenticators,
+//   }) as Adapter,
+//   providers: [],
+// });
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 export * from "./resend";
 export * from "./schemas";
 export * from "./stripe";
-export * from "./auth";
+// export * from "./auth";
 export * from "./current-user";
 export * from "./db";
 export * from "./action";
