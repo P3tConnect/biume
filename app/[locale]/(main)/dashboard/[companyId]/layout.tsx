3c429ff8cc7859f3052129b9d3061ecca2a594ba
FrekiManagarm <mathchambaud@icloud.com>
2024-10-17 18:04:49 +0200

    `feat`(`paw-225`): `register and login logic`
    ---------------------------------------
    `WIP implement clerk`
    ---------------------------------------
    Commit made by : Mathieu Chambaud

--- app/[locale]/(main)/dashboard/[companyId]/layout.tsx
+++ app/[locale]/(main)/dashboard/[companyId]/layout.tsx
@@ -1,11 +1,20 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 import DashboardLayoutComponents from "@/components/dashboard/layout/dashboard-layout";
 
-const DashboardCompanyLayout = ({ member, owner, params }: { member: ReactNode, owner: ReactNode, params: { companyId: string } }) => {
-
+const DashboardCompanyLayout = ({
+  member,
+  owner,
+  params,
+}: {
+  member: ReactNode;
+  owner: ReactNode;
+  params: { companyId: string };
+}) => {
   return (
-    <DashboardLayoutComponents companyId={params.companyId}>{owner}</DashboardLayoutComponents>
-  )
-}
+    <DashboardLayoutComponents companyId={params.companyId}>
+      {owner}
+    </DashboardLayoutComponents>
+  );
+};
 
-export default DashboardCompanyLayout
+export default DashboardCompanyLayout;
