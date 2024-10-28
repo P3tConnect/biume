833b097d6e48c6b264dbb8bafb143de5d60b911c
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-10-28 21:14:56 +0100

    remove full calendar

--- components/dashboard/pages/timetable-page/calendar.tsx
+++ components/dashboard/pages/timetable-page/calendar.tsx
@@ -9,82 +9,16 @@ import {
   CardTitle,
 } from "@/components/ui";
 import { useEffect, useRef, useState } from "react";
-import { CalendarApi } from "@fullcalendar/core/index.js";
 import CalendarDropdown from "./components/calendar-dropdown";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
 import { logger } from "@/src/lib";
-import multiMonthPlugin from "@fullcalendar/multimonth";
-import timeGridDayPlugin from "@fullcalendar/timegrid";
 import { toast } from "sonner";
 import { useLocale } from "next-intl";
 
 const TimeTableFullWidth = () => {
-  const calendarRef = useRef<FullCalendar>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const [viewMode, setViewMode] = useState("month");
   const [currentMonth, setCurrentMonth] = useState("");
-  const [calendarApi, setCalendarApi] = useState<CalendarApi>();
   const locale = useLocale();
-
-  useEffect(() => {
-    const calendar = calendarRef.current;
-    if (calendar) {
-      setCalendarApi(calendar.getApi());
-    }
-  }, []);
-
-  useEffect(() => {
-    const calendar = calendarRef.current;
-    if (calendar) {
-      setCalendarApi(calendar.getApi());
-    }
-  }, []);
-
-  // !!! That useEffect is here to update the calendar size when the container changes
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(() => {
-      if (calendarRef.current) {
-        const calendarApi = calendarRef.current.getApi();
-        calendarApi.updateSize(); // Adjusts FullCalendar's size when the container changes
-      }
-    });
-
-    if (containerRef.current) {
-      resizeObserver.observe(containerRef.current); // Observing the container
-    }
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, []);
-
-  // !!!
-
-  const handleNext = () => {
-    if (calendarApi) {
-      calendarApi.next();
-    } else {
-      logger.error("Calendar ref not found", calendarApi);
-      toast.error("Erreur lors de la navigation, veuillez réessayer plus tard");
-    }
-  };
-
-  const handlePrev = () => {
-    if (calendarApi) {
-      calendarApi.prev();
-    } else {
-      logger.error("Calendar ref not found", calendarApi);
-      toast.error("Erreur lors de la navigation, veuillez réessayer plus tard");
-    }
-  };
-
-  const handleChangeView = (view: string) => {
-    if (calendarApi) {
-      calendarApi.changeView(view);
-    }
-  };
 
   return (
     <Card
@@ -96,25 +30,6 @@ const TimeTableFullWidth = () => {
           Calendrier et prochains rendez-vous
         </CardTitle>
         <div className="flex flew-row items-center gap-2">
-          <Button
-            variant="default"
-            onClick={handlePrev}
-            className="rounded-full h-7 w-7 p-0"
-          >
-            <ArrowLeft size={18} />
-          </Button>
-          <Button
-            variant="default"
-            onClick={handleNext}
-            className="rounded-full h-7 w-7 p-0"
-          >
-            <ArrowRight size={18} />
-          </Button>
-          <p className="text-black dark:text-white">
-            {calendarApi && currentMonth != ""
-              ? `${currentMonth.toUpperCase()}`
-              : ""}
-          </p>
           <CalendarDropdown viewMode={viewMode} setViewMode={setViewMode} />
         </div>
       </CardHeader>
@@ -140,17 +55,6 @@ const TimeTableFullWidth = () => {
             <div className="rounded-full bg-red-500 h-4 w-4"></div>
             <h1>Annulée</h1>
           </div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <Button variant="default" onClick={() => { }} className="rounded-md">
-            Mois
-          </Button>
-          <Button variant="default" onClick={() => { }} className="rounded-md">
-            Semaine
-          </Button>
-          <Button variant="default" onClick={() => { }} className="rounded-md">
-            jours
-          </Button>
         </div>
       </div>
       <CardContent className="text-black dark:text-white">
