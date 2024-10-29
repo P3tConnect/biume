4af7ae09d59897d445886030815df60cf465964b
FrekiManagarm <mathchambaud@icloud.com>
2024-10-29 17:12:43 +0100

    wip progression widget

--- app/[locale]/(main)/dashboard/[companyId]/layout.tsx
+++ app/[locale]/(main)/dashboard/[companyId]/layout.tsx
@@ -3,6 +3,7 @@ import DashboardLayoutComponents from "@/components/dashboard/layout/dashboard-l
 import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from "@clerk/nextjs";
 import Loader from "@/components/loader";
 import { currentUser } from "@/src/lib";
+import { redirect } from "next/navigation";
 
 const DashboardCompanyLayout = async ({
   member,
@@ -11,17 +12,20 @@ const DashboardCompanyLayout = async ({
 }: {
   member: ReactNode;
   owner: ReactNode;
-  params: { companyId: string };
+  params: { companyId: string; locale: string };
 }) => {
+  const user = await currentUser();
+
+  console.log(user.publicMetadata.isPro, "isPro");
+
+  if (
+    user.publicMetadata.isPro == false ||
+    user.publicMetadata.isPro == undefined
+  ) {
+    redirect("/dashboard");
+  }
+
   return (
-    // <DashboardLayoutComponents companyId={params.companyId}>
-    //   <ClerkLoaded>
-    //     {owner}
-    //   </ClerkLoaded>
-    //   <ClerkLoading>
-    //     <Loader />
-    //   </ClerkLoading>
-    // </DashboardLayoutComponents>
     <DashboardLayoutComponents companyId={params.companyId}>
       {owner}
     </DashboardLayoutComponents>
--- app/[locale]/(main)/dashboard/layout.tsx
+++ app/[locale]/(main)/dashboard/layout.tsx
@@ -2,44 +2,20 @@ import DashboardLayoutComponents from "@/components/dashboard/layout/dashboard-l
 import { currentUser } from "@/src/lib";
 import { ReactNode } from "react";
 
-const DashboardLayout = async ({ client, children }: { children: ReactNode, client: ReactNode }) => {
+const DashboardLayout = async ({
+  children,
+  client,
+}: {
+  children: ReactNode;
+  client: ReactNode;
+}) => {
+  const user = await currentUser();
 
-  return (
-    <>
-      {children}
-    </>
-  )
-}
+  if (user.publicMetadata.isPro == true) {
+    return <>{children}</>;
+  }
 
-export default DashboardLayout
+  return <>{client}</>;
+};
 
-// import { ReactNode } from "react";
-// import DashboardLayoutComponents from "@/components/dashboard/layout/dashboard-layout";
-// import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from "@clerk/nextjs";
-// import Loader from "@/components/loader";
-
-// const DashboardCompanyLayout = ({
-//   member,
-//   owner,
-//   params,
-// }: {
-//   member: ReactNode;
-//   owner: ReactNode;
-//   params: { companyId: string };
-// }) => {
-//   return (
-//     // <DashboardLayoutComponents companyId={params.companyId}>
-//     //   <ClerkLoaded>
-//     //     {owner}
-//     //   </ClerkLoaded>
-//     //   <ClerkLoading>
-//     //     <Loader />
-//     //   </ClerkLoading>
-//     // </DashboardLayoutComponents>
-//     <DashboardLayoutComponents companyId={params.companyId}>
-//       {owner}
-//     </DashboardLayoutComponents>
-//   );
-// };
-
-// export default DashboardCompanyLayout;
+export default DashboardLayout;
