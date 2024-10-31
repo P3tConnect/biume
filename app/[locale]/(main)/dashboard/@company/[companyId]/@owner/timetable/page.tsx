5eedbe147dcf2808ba6686ed68f7c0c0544200e1
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2024-10-31 20:20:16 +0100

    resolv errors

--- app/[locale]/(main)/dashboard/@company/[companyId]/@owner/timetable/page.tsx
+++ app/[locale]/(main)/dashboard/@company/[companyId]/@owner/timetable/page.tsx
@@ -1,11 +1,7 @@
-import TimeTableFullWidth from "@/components/company-details/timetable-full-width";
-import TimeTablePageComponent from "@/components/dashboard/pages/timetable-page/timetable-page";
 import React from "react";
 
 const DashboardCompanyTimetablePage = () => {
-  return (
-    <TimeTablePageComponent />
-  );
+  return <>DashboardCompanyTimetablePage</>;
 };
 
 export default DashboardCompanyTimetablePage;
--- app/[locale]/(main)/dashboard/[companyId]/@owner/timetable/page.tsx
+++ app/[locale]/(main)/dashboard/[companyId]/@owner/timetable/page.tsx
@@ -1,11 +1,10 @@
-import TimeTableFullWidth from "@/components/company-details/timetable-full-width";
 import React from "react";
 
 const DashboardCompanyTimetablePage = () => {
   return (
     <>
       <div className="flex flex-col gap-5">
-        <TimeTableFullWidth />
+
       </div>
     </>
   );
