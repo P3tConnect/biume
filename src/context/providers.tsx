bb72f3e898207b7ef3631a2802858453f535fef5
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2024-12-11 19:30:55 +0100

    wip dockerfiles and modify Providers file

--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -1,26 +1,22 @@
-"use client"
+"use client";
 
-import { NextUIProvider } from '@nextui-org/react'
-import React, { PropsWithChildren } from 'react'
-import { ThemeProvider } from './theme-provider'
-import { useRouter } from 'next/navigation'
-import { Toaster } from 'sonner'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NextUIProvider } from "@nextui-org/react";
+import React, { PropsWithChildren } from "react";
+import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const Providers = async ({ children }: PropsWithChildren) => {
-  const router = useRouter();
+const Providers = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute='class' forcedTheme='dark' enableSystem={false}>
-        <NextUIProvider navigate={router.push}>
-          {children}
-          <Toaster />
-        </NextUIProvider>
+      <ThemeProvider attribute="class" forcedTheme="dark" enableSystem={false}>
+        {children}
+        <Toaster />
       </ThemeProvider>
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
