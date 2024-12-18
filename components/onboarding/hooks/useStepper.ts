7f809a77fa6edfa244c28ee94b3fe90cb6dc5a63
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-12-18 22:31:39 +0100

    wip onboarding pro

--- components/onboarding/hooks/useStepper.ts
+++ components/onboarding/hooks/useStepper.ts
@@ -5,30 +5,30 @@ export const { steps, useStepper } = defineStepper(
   {
     id: "informations",
     title: "Informations",
-    description: "",
+    description: "Renseignez les informations de votre entreprise pour que nous puissions créer le compte de votre établissement.",
     schema: z.object({})
   },
   {
     id: "services",
     title: "Services",
-    description: "",
+    description: "Ici vous pouvez choisir les services que vous offrez à vos clients.",
     schema: z.object({})
   },
   {
     id: "options",
     title: "Options",
-    description: "",
+    description: "Les options peuvent vous permettre de personnaliser votre offre de services et ainsi attirer davantage de clients.",
     schema: z.object({})
   },
   {
     id: "documents",
     title: "Documents",
-    description: "",
+    description: "Les documents nous permettent de vous identifier et de vous authentifier en tant que professionnel, vous pourrez les modifier à tout moment dans les réglages du compte de votre entreprise.",
     schema: z.object({})
   },
   {
     id: "complete",
     title: "Complete",
-    description: "",
+    description: "Vous avez terminé l'inscription, vous pouvez maintenant commencer à utiliser votre compte professionnel !",
   }
 );
