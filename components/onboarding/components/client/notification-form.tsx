e15da681585c25f165a1afa9d2434e17fe6a1e2b
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-03 15:13:52 +0100

    feature/paw-15: wip onboarding client, add ajustments for from notifications

--- components/onboarding/components/client/notification-form.tsx
+++ components/onboarding/components/client/notification-form.tsx
@@ -11,12 +11,10 @@ import {
 import { Checkbox } from "@/components/ui";
 import { CreateUserSchema } from "@/src/db/user";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const ClientNotificationForm = () => {
-  const [checked, setChecked] = useState(false);
   const form = useForm<z.infer<typeof CreateUserSchema>>({
     resolver: zodResolver(CreateUserSchema),
     defaultValues: {
@@ -39,7 +37,12 @@ const ClientNotificationForm = () => {
             <FormItem className="flex flex-row justify-start items-start">
               <div className="flex flex-row items-center gap-3">
                 <FormControl>
-                  <Checkbox {...field} checked={field.value} />
+                  <Checkbox
+                    checked={field?.value ?? undefined}
+                    onCheckedChange={(checked) =>
+                      field.onChange(checked === true)
+                    }
+                  />
                 </FormControl>
                 <FormLabel className="text-sm font-semibold">
                   Choisir de recevoir les notifications par SMS
@@ -56,7 +59,12 @@ const ClientNotificationForm = () => {
             <FormItem className="flex flex-row justify-start items-start">
               <div className="flex flex-row items-center gap-3">
                 <FormControl>
-                  <Checkbox {...field} checked={field.value} />
+                  <Checkbox
+                    checked={field?.value ?? undefined}
+                    onCheckedChange={(checked) =>
+                      field.onChange(checked === true)
+                    }
+                  />
                 </FormControl>
                 <FormLabel className="text-sm font-semibold">
                   Choisir de recevoir les notifications par email
