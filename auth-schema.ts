cdf71314fcab16842ff220c742752335225217b6
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-01-05 14:22:38 +0100

    wip onboarding company

--- auth-schema.ts
+++ auth-schema.ts
@@ -1,69 +1,80 @@
 import {
-  boolean,
   pgTable,
   text,
+  integer,
   timestamp,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: text("id").primaryKey(),
+  name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  emailVerified: boolean("emailVerified").notNull(),
+  emailVerified: boolean("email_verified").notNull(),
   image: text("image"),
-  createdAt: timestamp("createdAt").notNull(),
-  updatedAt: timestamp("updatedAt").notNull(),
-  twoFactorEnabled: boolean("twoFactorEnabled"),
-  isPro: boolean("isPro").notNull(),
-  onBoardingComplete: boolean("onBoardingComplete").notNull(),
-  stripeId: text("stripeId"),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  twoFactorEnabled: boolean("two_factor_enabled"),
+  firstname: text("firstname").notNull(),
+  lastname: text("lastname").notNull(),
+  isPro: boolean("is_pro"),
+  onBoardingComplete: boolean("on_boarding_complete"),
+  stripeId: text("stripe_id"),
+  address: text("address"),
+  zipCode: text("zip_code"),
+  country: text("country"),
+  city: text("city"),
+  phoneNumber: text("phone_number"),
+  smsNotification: boolean("sms_notification"),
+  emailNotification: boolean("email_notification"),
 });
 
 export const sessions = pgTable("sessions", {
   id: text("id").primaryKey(),
-  expiresAt: timestamp("expiresAt").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
   token: text("token").notNull().unique(),
-  createdAt: timestamp("createdAt").notNull(),
-  updatedAt: timestamp("updatedAt").notNull(),
-  ipAddress: text("ipAddress"),
-  userAgent: text("userAgent"),
-  userId: text("userId")
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
     .notNull()
     .references(() => users.id),
-  activeOrganizationId: text("activeOrganizationId"),
+  activeOrganizationId: text("active_organization_id"),
 });
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
-  accountId: text("accountId").notNull(),
-  providerId: text("providerId").notNull(),
-  userId: text("userId")
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
     .notNull()
     .references(() => users.id),
-  accessToken: text("accessToken"),
-  refreshToken: text("refreshToken"),
-  idToken: text("idToken"),
-  accessTokenExpiresAt: timestamp("accessTokenExpiresAt"),
-  refreshTokenExpiresAt: timestamp("refreshTokenExpiresAt"),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
   scope: text("scope"),
   password: text("password"),
-  createdAt: timestamp("createdAt").notNull(),
-  updatedAt: timestamp("updatedAt").notNull(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
 });
 
 export const verifications = pgTable("verifications", {
   id: text("id").primaryKey(),
   identifier: text("identifier").notNull(),
   value: text("value").notNull(),
-  expiresAt: timestamp("expiresAt").notNull(),
-  createdAt: timestamp("createdAt"),
-  updatedAt: timestamp("updatedAt"),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at"),
+  updatedAt: timestamp("updated_at"),
 });
 
-export const twoFactors = pgTable("twoFactors", {
+export const twoFactors = pgTable("two_factors", {
   id: text("id").primaryKey(),
   secret: text("secret").notNull(),
-  backupCodes: text("backupCodes").notNull(),
-  userId: text("userId")
+  backupCodes: text("backup_codes").notNull(),
+  userId: text("user_id")
     .notNull()
     .references(() => users.id),
 });
@@ -73,32 +84,32 @@ export const organizations = pgTable("organizations", {
   name: text("name").notNull(),
   slug: text("slug").unique(),
   logo: text("logo"),
-  createdAt: timestamp("createdAt").notNull(),
+  createdAt: timestamp("created_at").notNull(),
   metadata: text("metadata"),
 });
 
 export const members = pgTable("members", {
   id: text("id").primaryKey(),
-  organizationId: text("organizationId")
+  organizationId: text("organization_id")
     .notNull()
     .references(() => organizations.id),
-  userId: text("userId")
+  userId: text("user_id")
     .notNull()
     .references(() => users.id),
   role: text("role").notNull(),
-  createdAt: timestamp("createdAt").notNull(),
+  createdAt: timestamp("created_at").notNull(),
 });
 
 export const invitations = pgTable("invitations", {
   id: text("id").primaryKey(),
-  organizationId: text("organizationId")
+  organizationId: text("organization_id")
     .notNull()
     .references(() => organizations.id),
   email: text("email").notNull(),
   role: text("role"),
   status: text("status").notNull(),
-  expiresAt: timestamp("expiresAt").notNull(),
-  inviterId: text("inviterId")
+  expiresAt: timestamp("expires_at").notNull(),
+  inviterId: text("inviter_id")
     .notNull()
     .references(() => users.id),
 });
