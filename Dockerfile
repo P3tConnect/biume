5c82eb64b7e61e96eb1e24b99afb27e102b8d24a
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-09 16:30:04 +0100

    main: somes changes in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -84,4 +84,4 @@ EXPOSE 3000
 
 ENV PORT 3000
 
-CMD HOSTNAME=0.0.0.0 node server.js
+CMD HOSTNAME=0.0.0.0 bun run start
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from "stripe";
 import { safeConfig } from "./env";
 
 export const stripe = new Stripe(safeConfig.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2024-12-18.acacia" as any,
 });
 
