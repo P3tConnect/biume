b9a9087073f233b6dfd2ae1005730dd07fe0dc7f
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-09 14:36:29 +0100

    main: somes changes in dockerfile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ import { withUt } from 'uploadthing/tw'
 
 const darkMode = ["class"]
 const content = [
-    './node_modules/@uploadthing/react/dist/**/*',
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
