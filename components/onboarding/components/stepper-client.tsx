a6e48f2b1ffa61ccdbb243ba241ecd9c0c06cbd6
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-09 11:20:19 +0100

    feature/paw-15: added modifications to correct types and error handling on the onboarding form

--- components/onboarding/components/stepper-client.tsx
+++ components/onboarding/components/stepper-client.tsx
@@ -149,14 +149,23 @@ const StepperClient = ({ open }: { open: boolean }) => {
 };
 
 export const clientOnBoardingSchema = z.object({
-  image: z.string().url().optional(),
-  address: z.string(),
-  city: z.string(),
-  country: z.string(),
-  zipCode: z.string(),
-  phoneNumber: z.string(),
-  emailNotifications: z.boolean().default(false),
-  smsNotifications: z.boolean().default(false),
+  image: z.string().optional(),
+  address: z
+    .string()
+    .min(
+      1,
+      'Votre adresse doit contenie le numéro de votre rue ainsi que le nom de la rue'
+    ),
+  country: z.string().min(1, 'Le pays doit être valide'),
+  city: z.string().min(1, 'Votre ville doit être valide'),
+  zipCode: z
+    .string()
+    .min(5, 'Votre code postal doit être valide, soit 5 chiffres'),
+  phoneNumber: z
+    .string()
+    .min(10, 'Votre numéro doit comprendre que 10 chiffres'),
+  emailNotifications: z.boolean().default(false).optional(),
+  smsNotifications: z.boolean().default(false).optional(),
 });
 
 export default StepperClient;
