7787d61f5f2eb13c09fa0a053285a3dcbd7988d1
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-12 15:51:06 +0100

    feature/paw-17: wip dashboard client

--- app/(main)/dashboard/@client/page.tsx
+++ app/(main)/dashboard/@client/page.tsx
@@ -6,7 +6,6 @@ import StepperClient from '@/components/onboarding/components/stepper-client';
 import ClientTimetableWidget from '@/components/dashboard/shortcuts/client/client-timetable-widget';
 import ClientPetsWidget from '@/components/dashboard/shortcuts/client/client-pets-widget';
 import ClientHistoryReservationWidget from '@/components/dashboard/shortcuts/client/client-history-reservation-widget';
-import ClientStatsWidget from '@/components/dashboard/shortcuts/client/client-stats-widget';
 import ClientObservationsWidget from '@/components/dashboard/shortcuts/client/client-observations-widget';
 
 const ClientDashboardHomePage = () => {
@@ -19,14 +18,11 @@ const ClientDashboardHomePage = () => {
       {showModal && <StepperClient open={showModal} />}
       <div className='flex flex-col gap-4'>
         <div className='grid gap-4 lg:grid-cols-12'>
-          <div className='lg:col-span-4'>
+          <div className='lg:col-span-6'>
             <ClientTimetableWidget />
           </div>
-          <div className='lg:col-span-4'>
+          <div className='lg:col-span-6'>
             <ClientPetsWidget />
-          </div>
-          <div className='lg:col-span-4'>
-            <ClientStatsWidget />
           </div>
           <div className='lg:col-span-6'>
             <ClientHistoryReservationWidget />
