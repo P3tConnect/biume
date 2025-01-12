9218f71902b6ddb37f3499b4eb4740e2122bcda1
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-01-12 16:01:10 +0100

    update redirect with user on sign in and sign up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -6,7 +6,7 @@ import { signIn } from "@/src/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -17,7 +17,6 @@ const LoginPage = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const { handleSubmit, register } = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema),
     resolver: zodResolver(loginSchema),
   });
 
@@ -34,7 +33,8 @@ const LoginPage = () => {
         },
         onSuccess: (ctx) => {
           console.log(ctx, "ctx");
-          router.push(`/dashboard/client/${ctx.data.id}`);
+          toast.success("Connexion réussie ! Vous allez être redirigé ...");
+          router.push(`/dashboard/user/${ctx.data.user.id}`);
         },
         onError: (error: ErrorContext) => {
           setLoading(false);
