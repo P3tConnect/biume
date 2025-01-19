3da89984fd240befc4d5a1039b0e1811e085c0a7
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-01-19 16:13:05 +0100

    main: nothing important just an error ctrl + S eslint actif

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,16 +1,16 @@
 // import { Bubbles } from "@/components/landing/bubbles";
-import { CallToAction } from "@/components/landing/cta-section";
-import FeaturesSection from "@/components/landing/features";
-import Footer from "@/components/landing/footer";
-import Header from "@/components/landing/header";
-import Hero from "@/components/landing/hero";
-import Problem from "@/components/landing/problem";
-import { Pricing } from "@/components/landing/pricings";
-import FAQ from "@/components/landing/faq";
+import { CallToAction } from '@/components/landing/cta-section';
+import FeaturesSection from '@/components/landing/features';
+import Footer from '@/components/landing/footer';
+import Header from '@/components/landing/header';
+import Hero from '@/components/landing/hero';
+import Problem from '@/components/landing/problem';
+import { Pricing } from '@/components/landing/pricings';
+import FAQ from '@/components/landing/faq';
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen">
+    <main className='w-screen h-screen'>
       <Header />
       <Hero />
       <Problem />
