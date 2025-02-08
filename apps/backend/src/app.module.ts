1aba03b5bb7174495101d2935654991e6edf486f
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-02-08 14:07:17 +0100

    push

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { RemixController } from './remix/remix.controller';
 import { RemixService } from './remix/remix.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
   controllers: [RemixController],
-  providers: [RemixService],
+  providers: [RemixService, PrismaService],
 })
 export class AppModule {}
