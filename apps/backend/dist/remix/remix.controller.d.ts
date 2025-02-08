f57b9bde39c3e4b46e8b1b3593b7ebef7dbc922a
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-02-08 13:09:18 +0100

    push

--- apps/backend/dist/remix/remix.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-import { RemixService } from './remix.service';
-export declare class RemixController {
-    private readonly remixService;
-    constructor(remixService: RemixService);
-    handler(request: Request, response: Response, next: NextFunction): Promise<void>;
-}
--- packages/shared/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ViteDevServer } from 'vite';
-export declare const getPublicDir: () => string;
-export declare function startDevServer(app: any): Promise<ViteDevServer | undefined>;
-export declare function getServerBuild(): Promise<any>;
