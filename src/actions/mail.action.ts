5b5296cd653f5caf4d7b239ba718bdc0c9c2639a
Mathieu Chambaud <mathchambaud2000@gmail.com>
2025-03-09 09:45:03 +0100

    add organization hooks before create and before delete to handle customer and account creation and deletion on stripe

--- src/actions/mail.action.ts
+++ src/actions/mail.action.ts
@@ -10,7 +10,7 @@ export const newSubWaitList = createServerAction(
   emailSchema,
   async (input, ctx) => {
     const mail = await resend.emails.send({
-      from: "PawThera<contact@pawthera.com>",
+      from: "Biume <contact@biume.com>",
       subject: "New person in the WaitList",
       to: ["mathieu.chambaud@pawthera.com", "graig.kolodziejczyk@pawthera.com"],
       react: NewPersonWaitList({ subEmail: input.email }),
